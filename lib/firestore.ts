2dd2dcef52f77509eb5957a24ef852a090d6d43b
clieantapp-crypto <clieantapp@gmail.com>
2025-12-23 09:10:24 +0300

    HH

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,12 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5ETR3k0tqExX1ibV_wjOHFWAZ1upRuG4",
-  authDomain: "coffee-spark-ai-barista-9f911.firebaseapp.com",
-  projectId: "coffee-spark-ai-barista-9f911",
-  storageBucket: "coffee-spark-ai-barista-9f911.firebasestorage.app",
-  messagingSenderId: "563277943028",
-  appId: "1:563277943028:web:2a13b27270a76123240ea1"
+  apiKey: "AIzaSyBEMutxISSdHbL4OotcoKMh1Zv603jWzgw",
+  authDomain: "mynewbb-73847.firebaseapp.com",
+  databaseURL: "https://mynewbb-73847-default-rtdb.firebaseio.com",
+  projectId: "mynewbb-73847",
+  storageBucket: "mynewbb-73847.firebasestorage.app",
+  messagingSenderId: "1017329682260",
+  appId: "1:1017329682260:web:7c8e6a9ece4e91399ceac1",
+  measurementId: "G-E5XV1B9R32"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
